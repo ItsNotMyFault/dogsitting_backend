bf678d66532107c2a1997cf4db3aa415231dd5b0
aguay <aguay@imarcom.net>
2024-04-09 14:39:29 -0400

    add program

---
+++ Program.cs
@@ -0,0 +1,61 @@
+using dogsitting_backend.ApplicationServices;
+using dogsitting_backend.Domain;
+using dogsitting_backend.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Localization;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+
+IConfigurationRoot Configuration = builder.Configuration;
+IWebHostEnvironment Environment = builder.Environment;
+
+var isdev = Environment.IsDevelopment();
+
+builder.Services.AddSingleton<IConfiguration>(Configuration);
+builder.Services.AddTransient<TeamService>();
+builder.Services.AddTransient<TeamSQLRepository>();
+builder.Services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+builder.Services.AddDbContext<DogsittingDBContext>(options =>
+{
+    var connetionString = builder.Configuration.GetSection("ConnectionString:Dev:dogsitting").Value;
+    //options.UseMySQL();
+    options.UseMySql(connetionString, ServerVersion.AutoDetect(connetionString));
+}
+);
+
+builder.Services.AddMvc();
+builder.Logging.AddConsole();
+
+
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<DogsittingDBContext>();
+    context.Database.EnsureCreated();
+    // DbInitializer.Initialize(context);
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
